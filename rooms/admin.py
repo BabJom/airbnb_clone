16cff1504bdeed789990f8f8676cb2d5c3d42d8f
BobJom <liveros@naver.com>
2020-05-28 05:21:10 +0900

    Models and Query Set

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,6 +13,20 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition"""
 
+    fieldsets = (
+        ("Space", {"fields": ("guests", "beds", "bedrooms", "bathrooms",)},),
+        ("Basic Info", {"fields": ("name", "description", "address", "price",)}),
+        ("Times", {"fields": ("check_in", "check_out", "instant_books",)}),
+        (
+            "More About the Spaces",
+            {"fields": ("amenities", "facilities", "house_rule",)},
+        ),
+        ("Last Details", {"classes": ("collapse",), "fields": ("host",)}),
+    )
+    ordering = (
+        "name",
+        "price",
+    )
     list_display = (
         "name",
         "country",
@@ -26,15 +40,33 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_books",
+        "count_amenities",
     )
     list_filter = (
+        "host__superhost",
         "instant_books",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rule",
         "city",
         "country",
     )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rule",
+    )
     search_fields = ("city", "^host__username")
     readonly_fields = ("updated", "created")
-    pass
+
+    def count_amenities(self, object):
+        #    print(object.amenities.count())
+        return object.amenities.count()
+
+
+#    count_amenities.short_description = "Sexy"
 
 
 @admin.register(models.Photo)
